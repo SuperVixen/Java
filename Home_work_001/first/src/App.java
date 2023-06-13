7ac34968e65cb4709cbf9fd016f056f615c0a9db
Петрунёв Егор Леонидович <90984218+SuperVixen@users.noreply.github.com>
2023-06-13 09:10:25 +0200

    Calculator

--- Home_work_001/first/src/App.java
+++ Home_work_001/first/src/App.java
@@ -1,5 +1,49 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        // task1();
+        // task3();
+        // task4();
+        // task5();
+        calculator();
+        // System.out.println("Hello, World!");
+    }
+
+    public static void calculator() {// ничего на вход не передаём, спрашиваем у user'а числа и операцию
+ 
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter 1st number: ");
+        String numberOne = scan.nextLine();
+        System.out.println("Enter 2nd number: ");
+        String numberTwo = scan.nextLine();
+        System.out.println("Enter opertion symbol (+, -, * , /): ");
+        String operation = scan.nextLine();
+        scan.close();
+
+        System.out.println(numberOne + " " + operation + " " + numberTwo + " = " + mathematics(numberOne, numberTwo, operation));    
+    }
+
+    private static float mathematics(String a, String b, String c) {
+        float result = 0; 
+        float aNumeric = Float.parseFloat(a);
+        float bNumeric = Float.parseFloat(b);
+        switch (c) {
+            case "+":
+                result = aNumeric + bNumeric;
+                break;
+            case "-":
+                result = aNumeric - bNumeric;
+                break;
+            case "*":
+                result = aNumeric * bNumeric;
+                break;
+            case "/":
+                result = aNumeric / bNumeric;
+                break;
+            default: System.out.printf("Error! Enter correct operator");
+                break;
+        }
+        return result;
     }
 }
