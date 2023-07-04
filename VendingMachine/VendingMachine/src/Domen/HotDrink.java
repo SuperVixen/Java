12a4643cb0280124fdf3a77c2a31774bee42f3cb
SuperVixen <SuperVixen@xaker.ru>
2023-07-04 05:19:26 +0300

    HotDrink complete

---
+++ VendingMachine/VendingMachine/src/Domen/HotDrink.java
@@ -0,0 +1,29 @@
+package Domen;
+
+public class HotDrink extends Product {
+    // Поля
+    private int hotDrintTemp;
+// Свойства
+    public int getHotDrintTemp() {
+        return hotDrintTemp;
+    }
+
+    public void setHotDrintTemp(int hotDrintTemp) {
+        this.hotDrintTemp = hotDrintTemp;
+    }
+    //конструкторы
+    public HotDrink(int price, int place, String name, long id){
+        super(price,place,name,id);
+        this.hotDrintTemp = 40;
+    }
+    public HotDrink (int price, int place, String name, long id, int temp){
+        super(price,place,name,id);
+        this.hotDrintTemp = temp;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + "temperature = "+hotDrintTemp +"\n" ;
+    }
+
+}
